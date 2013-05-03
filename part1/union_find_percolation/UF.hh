a440f3cf12007d9a605bf24f54118592a781007d
Cesar Leblic <cesar.leblic@gmail.com>
2013-05-03 14:35:53 +0200

    percolation improvments

--- part1/union_find_percolation/UF.hh
+++ part1/union_find_percolation/UF.hh
@@ -6,13 +6,13 @@
 #include		<allegro5/allegro_primitives.h>
 #include		<iostream>
 
-# define		WIN_S 1000
+# define		WIN_S 1000.0f
 
 template		<int N>
 class UF
 {
 public:
-  UF<N>() :
+  UF<N>(bool improved = false) :
   size_(N)
   {
     //////////////////
@@ -31,23 +31,37 @@ public:
 	abort();
       }
     al_init_primitives_addon();
+
+
     ///////////////
     // init data //
     ///////////////
-    for (int i = 0; i < N * N + 2; ++i)
+    this->start_ = N * N;
+    this->end_ = N * N + 1;
+    this->improved_ = improved;
+    if (!(this->list_ = new int[N * N + 2])
+	|| !(this->wg_ = new unsigned int[N * N + 2])
+	|| !(this->open_ = new unsigned int[N * N + 2]))
+      {
+	std::cout << "Out of memory" << std::endl;
+      }
+    for (int i = 0; i < this->end_ + 1; ++i)
       {
 	this->list_[i] = i;
-	this->open_[i] = -1;
+	this->open_[i] = 0;
 	this->wg_[i] = 1;
       }
-    this->cellSize_ = WIN_S / N;
-    this->open_[N * N] = 0;
-    this->open_[N * N + 1] = 0;
+    this->cellSize_ =  WIN_S / N;
+    this->open_[this->start_] = 1;
+    this->open_[this->end_] = 1;
     srand(time(NULL));
   }
 
   ~UF<N>()
   {
+    delete this->list_;
+    delete this->wg_;
+    delete this->open_;
     al_shutdown_primitives_addon();
     al_destroy_display(this->display_);
   }
@@ -80,44 +94,45 @@ public:
   {
     while (i != this->list_[i])
       {
-	i = this->list_[i];
+	if (this->improved_)
+	  i = this->list_[this->list_[i]];
+	else
+	  i = this->list_[i];
       }
     return i;
   }
-
-
 
   bool			open(int x, int y)
   {
     int			p = y * this->size_ + x;
 
-    if (this->open_[p] != -1)
+    if (this->open_[p])
       return false;
-    this->open_[p] = 0;
+    this->open_[p] = 1;
 
     // union left
-    if (x > 0 && this->open_[p - 1] != -1)
+    if (x > 0 && this->open_[p - 1])
       this->setUnion(p, p - 1);
 
     // union right
-    if (x < this->size_ - 1 && this->open_[p + 1] != -1)
+    if (x < this->size_ - 1 && this->open_[p + 1])
       this->setUnion(p, p + 1);
 
     // union up
-    if (y > 0 && this->open_[p - this->size_] != -1)
+    if (y > 0 && this->open_[p - this->size_])
       this->setUnion(p, p - this->size_);
 
     // union down
-    if (y < this->size_ - 1 && this->open_[p + this->size_] != -1)
+    if (y < this->size_ - 1 && this->open_[p + this->size_])
       this->setUnion(p, p + this->size_);
 
     // union start
     if (y == 0)
-      this->setUnion(p, this->size_ * this->size_);
+      this->setUnion(p, this->start_);
 
     // union end
     if (y == this->size_ - 1)
-      this->setUnion(p, this->size_ * this->size_ + 1);
+      this->setUnion(p, this->end_);
 
     return true;
   }
@@ -128,34 +143,22 @@ public:
     int			x;
     int			y;
 
-    al_clear_to_color(al_map_rgb(54, 210, 65));
-    for (int i = 0; i < this->size_ * this->size_; ++i)
+    for (int i = 0; i < this->start_; ++i)
       {
 	x = (i % this->size_) * this->cellSize_;
 	y = (i / this->size_) * this->cellSize_;
-	if (this->open_[i] == -1)
+
+	if (!this->open_[i])
 	  {
-	    al_draw_filled_rectangle(x,
-				     y,
-				     x + this->cellSize_,
-				     y + this->cellSize_,
-				     al_map_rgb(0, 0, 0));
+	    al_draw_filled_rectangle(x, y, x + this->cellSize_, y + this->cellSize_, al_map_rgb(0, 0, 0));
 	    continue;
 	  }
-	else if (this->isConnected(i, this->size_ * this->size_))
+	else if (this->isConnected(i, this->start_))
 	  {
-	    al_draw_filled_rectangle(x,
-				     y,
-				     x + this->cellSize_,
-				     y + this->cellSize_,
-				     al_map_rgb(50, 10, 255));
+	    al_draw_filled_rectangle(x, y, x + this->cellSize_, y + this->cellSize_, al_map_rgb(50, 10, 255));
 	    continue;
 	  }
-	al_draw_filled_rectangle(x,
-				 y,
-				 x + this->cellSize_,
-				 y + this->cellSize_,
-				 al_map_rgb(255, 255, 255));
+	al_draw_filled_rectangle(x, y, x + this->cellSize_, y + this->cellSize_, al_map_rgb(255, 255, 255));
       }
     al_flip_display();
   }
@@ -163,23 +166,32 @@ public:
 
   void			run(bool display = false)
   {
-    while (!this->isConnected(this->size_ * this->size_, this->size_ * this->size_ + 1))
+    double		time = al_get_time();
+
+    while (!this->isConnected(this->start_, this->end_))
       {
 	while (!this->open(rand() % this->size_, rand() % this->size_))
 	  ;
 	if (display)
 	  this->draw();
       }
-    this->draw();
-    al_rest(6);
+    std::cout << "Map of " << this->size_ << "generated and calcutated in " << al_get_time() - time << " !" << std::endl;
+    if (this->size_ < 2000)
+      {
+	this->draw();
+	al_rest(6);
+      }
   }
 private:
   int			size_;
-  int			list_[N * N + 2];
-  int			open_[N * N + 2];
-  unsigned int		wg_[N * N + 2];
+  int			*list_;//[N * N + 2];
+  unsigned int		*open_;//[N * N + 2];
+  unsigned int		*wg_;//[N * N + 2];
+  int			start_;
+  int			end_;
   float			cellSize_;
   ALLEGRO_DISPLAY	*display_;
+  bool			improved_;
 };
 
 #endif			/* !UF_H_ */
--- part1/union_find_percolation/main.cpp
+++ part1/union_find_percolation/main.cpp
@@ -1,19 +1,23 @@
 #include			"UF.hh"
 
+// improuved take more time - strange ;)
+
 int				main()
 {
-  // 500 max
-
-  UF<500>			uf;
+  UF<5000>			uf(false);
 
   uf.run(false);
 
-  UF<100>			uf2;
+  UF<500>			uf1(false);
+
+  uf1.run(false);
+
+  UF<100>			uf2(false);
 
   uf2.run(false);
 
-  UF<50>			uf3;
+  UF<50>			uf3(false);
 
-  uf3.run(true);
+  uf3.run(false);
   return 0;
 }
